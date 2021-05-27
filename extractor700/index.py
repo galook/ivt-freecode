1adf061e4ea2016d15c2cfff0a3507757c899a72
Matej Holubec <matej.holubec@gmail.com>
2021-05-27 20:59:58 +0200

    feat: add comments and fix, now gives 100 pct

--- extractor700/index.py
+++ extractor700/index.py
@@ -1,12 +1,27 @@
-input = input().split("_")
+userInput = input()
+
+def desitky(numbers): # převede čísla např. 8, 4, 3 na 843
+    x = 0
+    i = 1
+    for num in numbers:
+        z = num * (10 ** (len(numbers) - i))
+        x += z
+        i += 1
+    return x
+
 numbers = []
-for word in input:
-    if (word.isdigit()):
-        length = len(word) - 1
-        finalNumber = 0
-        i = 0
-        for char in word:
-            finalNumber += ((ord(char) - 48) * (10 ** (length - i)))
+
+i = 0
+while i < len(userInput):  # projít písmeno po písmenu
+    if (userInput[i].isdigit()):   # pokud je znak číslo
+        num = []
+        while userInput[i].isdigit(): # dokud je stále znak s pořadím i od začátku číslo
+            num.append(ord(userInput[i]) - 48) # zkonvertovat ze znaku na číselnou hodnotu
             i += 1
-        numbers.append(finalNumber)
+            if(i >= len(userInput)): break;  # aby nespadl program, když je poslední znak číslo
+        numbers.append(desitky(num))
+    
+    else: i += 1
+
+
 print(sum(numbers))
