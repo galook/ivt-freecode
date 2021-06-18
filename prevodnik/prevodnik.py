7c4dbe0479fe08602377c6f89c88eb376a3a72ed
Matej Holubec <matej.holubec@gmail.com>
2021-06-18 09:19:47 +0200

    chre: format

--- prevodnik/prevodnik.py
+++ prevodnik/prevodnik.py
@@ -3,14 +3,11 @@ def baseCalculator(basero, numero):
         return "0"
     else:
         return baseCalculator(basero, numero//basero).lstrip("0") + "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"[numero%basero]
-
 res = []
-
 while True:
     base = int(input())
     if not base in range(2, 17):
          break
     number = int(input())
     res.append(str(baseCalculator(base, number)))
-    
 print('\n'.join(res))
